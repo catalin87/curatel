67335735e508fb8b02adfcfd6a13bd860b623de9
catalin <catalin87@gmail.com>
2025-01-04 17:39:08 +0200

    Static FE pages

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function onSubmit(token) {
-    console.log('submit', token);
-    document.getElementById("contact-form").submit();
-}
-
-function openNav() {
-    document.getElementById("meniuCuratel").classList.add("open");
-}
-
-function closeNav() {
-    document.getElementById("meniuCuratel").classList.remove("open");
-}
-
 document.querySelectorAll('.overlay-content a').forEach(link => {
     link.addEventListener('click', function (event) {
         event.preventDefault();
@@ -28,56 +15,7 @@ document.getElementById('defaultOpen').addEventListener('click', function() {
     openPage('curatenieGenerala', this, 'green');
 });
 
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-}
-
-function createSparkle() {
-    const sparkle = document.createElement('div');
-    sparkle.className = 'sparkle';
-    sparkle.style.top = `${100 + Math.random() * (window.innerHeight - 100)}px`;
-    sparkle.style.left = `${50 + Math.random() * (window.innerWidth - 100)}px`;
-    document.body.appendChild(sparkle);
-
-    // Use requestAnimationFrame for smoother animations
-    requestAnimationFrame(() => {
-        setTimeout(() => {
-            sparkle.remove();
-        }, 1000);
-    });
-}
-
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
 document.getElementById("defaultOpen").click();
-
-
 
 const sparkleInterval = setInterval(createSparkle, 300); // Reduced interval time to 300ms
 
